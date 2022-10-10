2e069fc2f692bfe8fe0b95d82ae777f7c15dfb9a
lianzi liu <lianziliu.sugar@gmail.com>
2022-10-10 14:49:27 -0400

    pass

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+let currentUser = 'Liu';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!' ;
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, '  + currentUser.toUpperCase() + '!' ;
+
+const shortGreeting ='Welcome, ' + currentUser[0] + '!' ;
